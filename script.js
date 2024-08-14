fc20872190d0d41fdbdb96db35a3aa15a011ea0f
Whitney <169212050+whitneybarr@users.noreply.github.com>
2024-08-14 18:16:52 -0500

    Progressively darken cells with each pass

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const button = document.querySelector("button")
 
 function drawGrid() {
 for (i = 0; i < gridSize ** 2; i++) {
+    let o = 0
     let cell = document.createElement("div")
     cell.setAttribute("class","cell")
     cell.setAttribute("style", `height: ${960/gridSize-3}px; width:${960/gridSize-3}px`)
     cell.addEventListener("mouseenter", () => {
         cell.setAttribute("class", "cellFilled")
-        cell.style.backgroundColor = `rgb(${Math.floor(Math.random() * (256 - 0))} ${Math.floor(Math.random() * 256)} ${Math.floor(Math.random() * 256)})`
+       cell.style.backgroundColor = `rgb(${Math.floor(Math.random() * (256 - 0))} ${Math.floor(Math.random() * 256)} ${Math.floor(Math.random() * 256)} / ${o+= .1})`
     })
     container.appendChild(cell);
 }}
